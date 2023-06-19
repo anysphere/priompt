f351453424876855a6fbd54acc9d36cce7568ab0
Arvid Lunnemark <arvid.lunnemark@gmail.com>
2023-06-18 23:14:55 -0700

    kill

--- dist/components.d.ts
+++
@@ -1,4 +0,0 @@
-import { PromptElement, PromptProps } from "./types";
-export declare function SystemMessage(props: PromptProps): PromptElement;
-export declare function UserMessage(props: PromptProps): PromptElement;
-export declare function AssistantMessage(props: PromptProps): PromptElement;
--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-export * from './lib';
-export * from './components';
-export { PreviewManager, dumpProps } from './preview';
-export type { PreviewManagerGetPromptQuery, PreviewConfig } from './preview';
-export type { JSX, Prompt, PromptElement, PromptProps } from './types';
