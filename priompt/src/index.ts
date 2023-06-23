d9558b75a0721066404d880f072a2c696b2ccc76
Arvid Lunnemark <arvid.lunnemark@gmail.com>
2023-06-23 10:58:44 -0700

    fix ai lint bug

--- priompt/src/index.ts
+++ priompt/src/index.ts
@@ -6,5 +6,3 @@ export { PreviewManager, dumpProps } from './preview';
 export type { PreviewManagerGetPromptQuery, PreviewConfig } from './preview';
 
 export type { JSX, Prompt, PromptElement, PromptProps } from './types';
-
-export type * from './types';
