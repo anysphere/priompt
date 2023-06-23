157324866a73896b100af7290b484d7931e73308
Arvid Lunnemark <arvid.lunnemark@gmail.com>
2023-06-23 10:55:01 -0700

    get the tests back in

--- priompt/src/index.ts
+++ priompt/src/index.ts
@@ -6,3 +6,5 @@ export { PreviewManager, dumpProps } from './preview';
 export type { PreviewManagerGetPromptQuery, PreviewConfig } from './preview';
 
 export type { JSX, Prompt, PromptElement, PromptProps } from './types';
+
+export type * from './types';
