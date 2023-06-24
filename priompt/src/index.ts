032ed97f333e93bbfaf9e142a4e2dc870b3366cf
Arvid Lunnemark <arvid.lunnemark@gmail.com>
2023-06-23 19:02:32 -0700

    do stuff

--- priompt/src/index.ts
+++ priompt/src/index.ts
@@ -5,4 +5,4 @@ export * from './components';
 export { PreviewManager, dumpProps } from './preview';
 export type { PreviewManagerGetPromptQuery, PreviewManagerLiveModeQuery, PreviewManagerLiveModeResultQuery, PreviewConfig } from './preview';
 
-export type { JSX, Prompt, PromptElement, PromptProps } from './types';
+export type { JSX, Prompt, PromptElement, PromptProps, ChatAndFunctionPromptFunction } from './types';
