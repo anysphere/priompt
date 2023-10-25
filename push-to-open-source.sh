cf489765dcd1fa2f08fe98f521ee5634bca45209
Arvid Lunnemark <arvid.lunnemark@gmail.com>
2023-10-24 20:14:26 -0700

    add initial open source script to sync
    Co-authored-by: Arvid Lunnemark <arvid.lunnemark@gmail.com>
    
    " >> "$SCRIPT_DIR/commit.template"
    
    " >> "$SCRIPT_DIR/commit.template"

--- push-to-open-source.sh
+++ push-to-open-source.sh
@@ -22,11 +22,56 @@ if [[ -n $(git status --porcelain) ]]; then
   git commit -m "update the eslintrc.base.json"
 fi
 
-# cd to the root everysphere folder
-cd "$SCRIPT_DIR"/../../..
+if [[ ! -d "$SCRIPT_DIR/priompt-opensource" ]]; then
+  git clone git@github.com:anysphere/priompt "$SCRIPT_DIR/priompt-opensource"
+fi
 
-git subtree push --prefix=backend/packages/priompt git@github.com:anysphere/priompt main
-git subtree split --rejoin --prefix=backend/packages/priompt
+cd "$SCRIPT_DIR/priompt-opensource"
+git checkout main
+git checkout -- . || true
+git restore --staged . || true
+git checkout -- . || true
+git clean -fd . || true
+git pull
+if [[ -n $(git status --porcelain) ]]; then
+  echo -e "${RED}Your git state inside priompt-opensource is not empty. Aborting the script...${NC}"
+  exit 1
+fi
+cd "$SCRIPT_DIR"
 
-echo "NOW REVIEW THE CHANGES AND PUSH TO GITHUB"
-echo "Run git push after reviewing git log NOW."
+cd "$SCRIPT_DIR/../../.."
+
+LAST_SYNCED_COMMIT=$(cat "$SCRIPT_DIR/../priompt-last-internal-synced-commit.txt")
+echo "LAST_SYNCED_COMMIT: $LAST_SYNCED_COMMIT"
+COMMIT_IDS=$(git rev-list --reverse HEAD...$LAST_SYNCED_COMMIT -- "backend/packages/priompt")
+
+
+echo "Commit IDs:"
+echo $COMMIT_IDS
+
+for COMMIT_ID in $COMMIT_IDS
+do
+  git show $COMMIT_ID -- "backend/packages/priompt" > "$SCRIPT_DIR/commit.patch"
+  sd '' '' "$SCRIPT_DIR/commit.patch"
+  cd "$SCRIPT_DIR/priompt-opensource"
+  git apply "$SCRIPT_DIR/commit.patch"
+  git add .
+  COMMIT_MSG=$(cd $SCRIPT_DIR && git log -1 --pretty=%B $COMMIT_ID | tr -d '\r')
+  echo "$COMMIT_MSG" > "$SCRIPT_DIR/commit.template"
+  echo -e "\n\n" >> "$SCRIPT_DIR/commit.template"
+  COMMIT_AUTHOR=$(cd $SCRIPT_DIR && git log -1 --pretty=%an $COMMIT_ID)
+  COMMIT_EMAIL=$(cd $SCRIPT_DIR && git log -1 --pretty=%ae $COMMIT_ID)
+  echo "Co-authored-by: $COMMIT_AUTHOR <$COMMIT_EMAIL>" >> "$SCRIPT_DIR/commit.template"
+  echo -e "\n\n" >> "$SCRIPT_DIR/commit.template"
+  FULL_COMMIT=$(cd $SCRIPT_DIR && cat "$SCRIPT_DIR/commit.patch")
+  echo "$FULL_COMMIT" | while IFS= read -r line
+  do
+    echo -e "# $line" >> "$SCRIPT_DIR/commit.template"
+  done
+  git commit --template="$SCRIPT_DIR/commit.template"
+  COMMIT_ID_OPENSOURCE=$(git rev-parse HEAD)
+  cd -
+  echo "$COMMIT_ID_OPENSOURCE" > "$SCRIPT_DIR/../priompt-last-open-source-synced-commit.txt"
+  echo "$COMMIT_ID" > "$SCRIPT_DIR/../priompt-last-internal-synced-commit.txt"
+done
+
