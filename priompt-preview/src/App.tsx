8ed70571ff387667777aaebe12b865f91c259edc
Arvid Lunnemark <arvid.lunnemark@gmail.com>
2024-01-31 10:30:44 -0800

    fix priompt playground.
    Co-authored-by: Arvid Lunnemark <arvid.lunnemark@gmail.com>

--- priompt-preview/src/App.tsx
+++ priompt-preview/src/App.tsx
@@ -61,8 +61,7 @@ function useDebouncedCallback<T extends (...args: A[]) => R, A, R>(
   ) as T;
 }
 
-const ALL_MODELS_STR =
-  "gpt-3.5-turbo,gpt-3.5-turbo-1106,gpt-4,gpt-4-32k,gpt-4-1106-preview";
+const ALL_MODELS_STR = "gpt-3.5-turbo,gpt-4,gpt-4-32k";
 const ALL_MODELS = ALL_MODELS_STR.split(",");
 const COMPLETION_MODELS_STR = "text-davinci-003,code-davinci-002";
 const COMPLETION_MODELS = COMPLETION_MODELS_STR.split(",");
