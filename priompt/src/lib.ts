6c7a7fe5d2e1a9ffab9834abfbc30d8d63ad4f25
Arvid Lunnemark <arvid.lunnemark@gmail.com>
2024-02-17 14:40:36 -0800

    fix typo in recursive call

--- priompt/src/lib.ts
+++ priompt/src/lib.ts
@@ -1878,7 +1878,7 @@ function validateUnrenderedPrompt(elem: PromptElement): void {
 function validateNoUnhandledTypes(elem: PromptElement): void {
 	if (Array.isArray(elem)) {
 		for (const child of elem) {
-			validateNoUnhandledTypes(child);
+			validateNotBothAbsoluteAndRelativePriority(child);
 		}
 		return;
 	}
