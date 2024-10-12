e70e78cd822ccf5f0700976e1b591ab0a463cadb
Arvid Lunnemark <arvid.lunnemark@gmail.com>
2024-10-12 14:21:26 -0700

    avoid dns.lookup for IPs inside hot-shots StatsD client
    added the workaround from
    https://github.com/brightcove/hot-shots/issues/198#issuecomment-1278379052
    
    Co-authored-by: Aman Karmani <aman@tmm1.net>

--- priompt/src/statsd.ts
+++ priompt/src/statsd.ts
@@ -1,4 +1,6 @@
 import { StatsD } from 'hot-shots';
+import { isIP } from 'net';
+import dns from 'dns';
 
 const statsdHost = (
 	process.env.DD_AGENT_HOST !== undefined
@@ -12,4 +14,14 @@ export const statsd = new StatsD({
 	port: 8125,
 	prefix: 'cursor.',
 	globalTags: { env: 'prod' },
+	udpSocketOptions: { // workaround for https://github.com/brightcove/hot-shots/issues/198
+		type: 'udp4',
+		lookup: (host, opts, callback) => {
+			if (isIP(host)) {
+				callback(null, host, 4);
+				return;
+			}
+			dns.lookup(host, opts, callback);
+		}
+	}
 });
